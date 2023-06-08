7b1edc2bd10270922b3d560c31b8aa1f6c80dd25
Daniel Sulkowski <dansulk@gmail.com>
2023-06-08 21:35:26 +0200

    added component UsersList

---
+++ src/components/UsersList.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Lista użytkowników</h1>
+  <div>
+    <ul>
+    <li v-for="user in users" :key="user.id">
+      {{user.name.first}} {{user.name.last}}
+    </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+
+  async mounted() {
+    await this.fetchUsers();
+  },
+
+  methods: {
+    async fetchUsers() {
+      try {
+        const response = await axios.get('https://randomuser.me/api/?results=10');
+        this.users=response.data.results;
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+}
+
+</script>
