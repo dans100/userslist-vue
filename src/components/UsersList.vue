83448e164ab286b350bb017add9d8281ed9b67f8
Daniel Sulkowski <dansulk@gmail.com>
2023-06-10 10:05:30 +0200

    added styles to users list

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -4,7 +4,7 @@
     <ul>
     <li v-for="user in users" :key="user.id">
       <img :src="user.picture.medium"/>
-          {{user.name.first}} {{user.name.last}}
+          <strong>{{user.name.first}} {{user.name.last}}</strong>
     </li>
     </ul>
   </div>
@@ -12,6 +12,7 @@
 
 <script>
 import axios from 'axios';
+
 
 export default {
   data() {
@@ -38,4 +39,66 @@ export default {
 
 </script>
 
+<style scoped>
 
+h1 {
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: 300;
+  letter-spacing: 2px;
+}
+
+div {
+  margin: 0 auto;
+  width: 600px;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #b8babe;
+  border-radius: 10px;
+  padding: 30px 0 30px 0;
+  box-shadow: 2px 2px 2px #b8babe;
+}
+
+
+ul {
+  list-style: none;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  visibility: hidden;
+}
+
+ul > * {
+  visibility: visible;
+  transition: opacity 0.5s ease;
+}
+
+ul:hover > :not(:hover) {
+  opacity: 0.3;
+}
+
+li {
+  width: 70%;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  background-color: #d7d8da;
+  border: 1px solid #b8babe;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+
+img {
+  width: 50px;
+  height: auto;
+  border-radius: 50%;
+  margin-right: 20px;
+}
+
+</style>
